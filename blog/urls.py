0adae7de538fa670c674e56f7fe6522530a09dc8
gur79 <gur79tub@gmail.com>
2019-01-11 17:41:31 +0200

    working prototype

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from blog.views import blog, another_one, done, article_details
 urlpatterns = [
     path('', blog, name='blog'),
     path('done/', done, name='done'),
-    path('<slug:slug>/', article_details, name='article-details'),
+    path('article/<slug:slug>/', article_details, name='article-details'),
     path('another_one/', another_one, name='another_one'),
 ]
