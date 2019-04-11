002efbb0ce1d5902e4a47693155e681f5c4d8450
bahatiphill <gur79tub@gmail.com>
2019-04-11 15:47:29 +0200

    cleared css and redesign

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from blog.views import blog, another_one,  article_details, edit_article
 
 urlpatterns = [
     path('', blog, name='blog'),
+    path('article/add/', another_one, name='another_one'),
     path('article/<slug:slug>/', article_details, name='article-details'),
     path('article/<slug:slug>/edit/', edit_article, name='article-edit'),
-    path('another_one/', another_one, name='another_one'),
     
 ]
